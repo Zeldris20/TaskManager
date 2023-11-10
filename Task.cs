75eb0d8375e353725253a5fa5e3b27c1d3205a3b
Zeldris20 <christianclapson@gmail.com>
2023-11-10 06:38:45 +0000

    Initial commit: Add Task, TaskManager, and Program classes

---
+++ Task.cs
@@ -0,0 +1,7 @@
+// Task.cs
+public class Task
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ TaskManager.cs
@@ -0,0 +1,38 @@
+// TaskManager.cs
+using System;
+using System.Collections.Generic;
+
+public class TaskManager
+{
+    private List<Task> tasks;
+
+    public TaskManager()
+    {
+        tasks = new List<Task>();
+    }
+
+    public void AddTask(string description)
+    {
+        int taskId = tasks.Count + 1;
+        Task newTask = new Task { Id = taskId, Description = description, IsComplete = false };
+        tasks.Add(newTask);
+    }
+
+    public void MarkTaskAsComplete(int taskId)
+    {
+        Task task = tasks.Find(t => t.Id == taskId);
+        if (task != null)
+        {
+            task.IsComplete = true;
+        }
+    }
+
+    public void ListTasks()
+    {
+        Console.WriteLine("Tasks:");
+        foreach (var task in tasks)
+        {
+            Console.WriteLine($"[{task.Id}] {task.Description} - {(task.IsComplete ? "Complete" : "Incomplete")}");
+        }
+    }
+}
