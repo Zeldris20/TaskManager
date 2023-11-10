f3b3f2968374b27956acc6878e6dfad8d9607d94
Zeldris20 <christianclapson@gmail.com>
2023-11-10 07:15:24 +0000

    Feature Add and Delete Tasks

--- Program.cs
+++ Program.cs
@@ -12,11 +12,24 @@ class Program
         taskManager.AddTask("Learn Git basics");
         taskManager.AddTask("Push changes to GitHub");
 
+        Console.WriteLine("Tasks: ");
+
+        // Allow the user to add tasks
+        Console.Write("Enter a new Task: ");
+        string userTaskDescription = Console.ReadLine();
+        taskManager.AddTask(userTaskDescription);
+
+        // Display tasks including the user-added task
+        taskManager.ListTasks();
+        // Allow the user to delete tasks
+        Console.Write("Enter the task number to delete: ");
+        if (int.TryParse(Console.ReadLine(), out int taskNumberToDelete))
+        {
+            taskManager.DeleteTask(taskNumberToDelete);
+        }
+        // Display tasks after deletion
         taskManager.ListTasks();
 
-        Console.WriteLine("\nMarking task 1 as complete...");
-        taskManager.MarkTaskAsComplete(1);
-
-        taskManager.ListTasks();
+        Console.ReadLine(); // Wait for user to press enter
     }
 }
