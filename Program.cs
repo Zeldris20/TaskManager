7f7169852e126994bbfc2d184c6f0413230fab4e
Zeldris20 <christianclapson@gmail.com>
2023-11-10 06:07:25 +0000

    Add updates for the feature

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program 
+{
+    static void Main()
+    {
+        TaskManager taskManager = new TaskManager();
+
+        // Example usage
+        taskManager.AddTask("Create a simple project in C#");
+        taskManager.AddTask("Learn Git basics");
+        taskManager.AddTask("Push changes to GitHub");
+
+        taskManager.ListTasks();
+
+        Console.WriteLine("\nMarking task 1 as complete...");
+        taskManager.MarkTaskAsComplete(1);
+
+        taskManager.ListTasks();
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,6 @@
+public class Task 
+{
+    public int Id {get; set; }
+    public string Description {get; set; }
+    public bool IsComplete { get; set; }
+}
